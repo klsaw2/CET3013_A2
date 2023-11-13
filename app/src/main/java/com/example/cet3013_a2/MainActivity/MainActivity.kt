348413c71e80ed36c2e4a587f60425bd53b11b4e
Oowu <brianlim.zyuan@gmail.com>
2023-11-13 17:47:00 +0800

    Add: MainActivity layouts

---
+++ app/src/main/java/com/example/cet3013_a2/MainActivity/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.cet3013_a2.MainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cet3013_a2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
