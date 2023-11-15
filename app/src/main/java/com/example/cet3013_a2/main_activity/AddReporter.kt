ad58a744e11333ec87b2ca5ed51e56b662b2946b
klsaw2 <inxsaw@gmail.com>
2023-11-15 15:22:55 +0800

    15/11/2023 V2

--- app/src/main/java/com/example/cet3013_a2/main_activity/AddReporter.kt
+++ app/src/main/java/com/example/cet3013_a2/main_activity/AddReporter.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.cet3013_a2.R
+import androidx.navigation.findNavController
+import com.example.cet3013_a2.databinding.FragmentAddReporterBinding
 
 class AddReporter  : Fragment() {
 
+    private var _binding: FragmentAddReporterBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_reporter, container, false)
+        _binding = FragmentAddReporterBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.cancelButton.setOnClickListener {
+            //view.findNavController().navigate()
+        }
+        binding.confirmButton.setOnClickListener {
+            TODO()
+        }
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/cet3013_a2/main_activity/EditReporter.kt
+++ app/src/main/java/com/example/cet3013_a2/main_activity/EditReporter.kt
@@ -6,14 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cet3013_a2.R
+import com.example.cet3013_a2.databinding.FragmentEditReporterBinding
 
 class EditReporter : Fragment() {
+
+    private var _binding: FragmentEditReporterBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_reporter, container, false)
+        _binding = FragmentEditReporterBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.cancelButton.setOnClickListener {
+            //view.findNavController().navigate()
+        }
+        binding.confirmButton.setOnClickListener {
+            TODO()
+        }
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
