2df4b98d039d32102d77ce7c592c1ea8797d8ca3
klsaw2 <inxsaw@gmail.com>
2023-11-15 11:58:05 +0800

    Update EditReporter.kt

--- app/src/main/java/com/example/cet3013_a2/main_activity/EditReporter.kt
+++ app/src/main/java/com/example/cet3013_a2/main_activity/EditReporter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cet3013_a2.R
 
-class ReportersFragment : Fragment() {
+class EditReporter : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
